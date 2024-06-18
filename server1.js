9cd445e267123b67c8b363c55c3ad84e6e008f96
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-06-18 19:04:03 +0530

    Added routes in server for better testing

--- server1.js
+++ server1.js
@@ -4,6 +4,12 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello from server on port 3001');
 });
+app.get('/health', (req, res) => {
+    res.send('Health checking on port 3001 successfull');
+});
+app.get('/api', (req, res) => {
+    res.send('api on port 3001 successfull');
+});
 
 app.listen(3001, () => {
     console.log('Server running on port 3001');
--- server3.js
+++ server3.js
@@ -4,6 +4,13 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello from server on port 3003');
 });
+app.get('/health', (req, res) => {
+    res.send('Health checking on port 3003 successfull');
+});
+app.get('/api', (req, res) => {
+    res.send('api on port 3003 successfull');
+});
+
 
 app.listen(3003, () => {
     console.log('Server running on port 3003');
